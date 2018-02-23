3c316298affa28d4003b063fcc9fd068e42bcc55
ANDREW\shanj <cmujiangshan@gmail.com>
2018-02-22 19:02:27 -0500

    1

---
+++ Pie/Assets/CallDraw.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CallDraw : MonoBehaviour {
+    public PieChartMesh pie;
+    /// <summary>
+    /// The Data that represent per chart size
+    /// </summary>
+    public float[] mData;
+    
+    public float mRotationAngle;
+    public float mRadius;
+	// Use this for initialization
+	void FixedUpdate () {
+        pie.Init(mData,100,mRotationAngle,mRadius);
+        pie.Draw();
+	}
+}
